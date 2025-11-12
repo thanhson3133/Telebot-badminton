90dcbb446c8030d70750180b7bbbf8bdaf8b0a8f
thanhson3133 <thanhson3133@gmail.com>
2025-11-12 13:34:41 +0700

    update change bot

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const botRoutes = require("./routes/botRoutes");
 
 mongoose.connection.once("open", () => {
   console.log("✅ MongoDB connected, starting bot...");
-  const bot = new Telegraf("8208782632:AAHOYslILkZc5bhSIZxpyF0qBYIYtfONinE");
+  const bot = new Telegraf("8004511736:AAFwcs8ry4XTf-awkUK2H8dqchgcH0_mr6Y");
   botRoutes(bot);
   bot.launch();
   console.log("✅ Bot launched");
