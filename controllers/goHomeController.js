e83348d85f3a0a685707fbbf43f2b303ad021943
thanhson3133 <thanhson3133@gmail.com>
2025-11-12 14:04:41 +0700

    update go home

--- controllers/goHomeController.js
+++ controllers/goHomeController.js
@@ -1,19 +1,38 @@
 const moment = require("moment-timezone");
 
 exports.timeToGoHome = (ctx) => {
+  const user = ctx.message.text?.split(" ")?.[1];
   const now = moment.tz("Asia/Ho_Chi_Minh");
-  const target = now.clone().hour(17).minute(30).second(0);
+
+  // Mặc định 17:30
+  let targetHour = 17;
+  let targetMinute = 30;
+
+
+  console.log(user);
+  
+  if (user?.toLowerCase() === "cuocthien") {
+    targetHour = 18;
+    targetMinute = 15;
+  }
+
+  const target = now.clone().hour(targetHour).minute(targetMinute).second(0);
 
   let diff = target.diff(now);
+
   if (diff <= 0) {
     ctx.reply("Hết giờ làm ✅", { reply_to_message_id: ctx.message.message_id });
     return;
   }
 
   const duration = moment.duration(diff);
-  const hours = Math.floor(duration.asHours());
+  const hours = duration.hours();
   const minutes = duration.minutes();
 
-  const msg = hours > 0 ? `${hours}h ${minutes} phút để tới 17:30 ⏳` : `${minutes} phút để tới 17:30 ⏳`;
+  const msg =
+    hours > 0
+      ? `${hours}h ${minutes} phút để tới ${targetHour}:${targetMinute} ⏳`
+      : `${minutes} phút để tới ${targetHour}:${targetMinute} ⏳`;
+
   ctx.reply(msg, { reply_to_message_id: ctx.message.message_id });
 };
